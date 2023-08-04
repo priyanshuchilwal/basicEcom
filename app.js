c1aeb4ea428e07519336b7bcf3283fa844fa309c
debhumi_pahadi <debhumi_pahadi@email.com>
2023-08-04 23:43:51 +0530

    Initial Commit

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 const ejs = require("ejs");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const authRoutes = require("./server/routes/authRoutes");
 const homeRoutes = require("./server/routes/homeRoutes");
@@ -13,12 +16,13 @@ const PORT = process.env.PORT || 3000;
 connectDB();
 
 app.use(express.static(path.join(__dirname, "public")));
-
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/auth", authRoutes);
 app.use(homeRoutes);
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect("mongodb://127.0.0.1:27017/basicEcom");
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
     console.log(`Database Connected: ${conn.connection.host}`);
   } catch (error) {
     console.log("Database Error: " + error);
